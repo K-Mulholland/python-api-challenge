02ce01b9c686f83e497db04944d42af992983577
K-Mulholland <105945908+K-Mulholland@users.noreply.github.com>
2022-11-27 21:11:15 -0500

    Add files via upload

---
+++ starter_code/api_keys.py
@@ -0,0 +1,8 @@
+# OpenWeatherMap API Key
+weather_api_key = "3cfdf1cb734c235fbb06bdb4ed6439be"
+
+# Google API Key
+g_key = "AIzaSyDoWyRR66l91drQJbAxlqSXkPjV659NKoA"
+
+#Geoapify API Key
+geoapify_key="d3b713a0dd2346ecae59f804d4b47c1e"
